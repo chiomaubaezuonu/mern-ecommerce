c52045d3500a9278a777e5dd25ca802bc4501165
chiomaubaezuonu <chiomaubaezuonu102@gmail.com>
2025-09-04 21:39:49 +0100

    Modified Casings for file names and imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
-
-import About from "./pages/About";
 import Home from "./pages/Home";
+import About from "./pages/About";
+
 
 
 function App() {
