0d479549a21959f6850628fc2e370223037a4b6b
chiomaubaezuonu <chiomaubaezuonu102@gmail.com>
2025-10-04 09:03:33 +0100

    Added user authentication routes for login

--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -1,6 +1,7 @@
 import express from "express"
 import bcrypt from "bcryptjs"
 import User from "../Model/UserModel.js"
+import jwt from "jsonwebtoken"
 
 const router = express.Router()
 
@@ -19,11 +20,42 @@ router.post("/signup", async (req, res) => {
             email,
             password: hashedPassword
         })
-        res.status(200).json({message: "User craeted successfully", user})
+        res.status(201).json({message: "User created successfully", user})
     } catch (error) {
         console.error(error)
         res.status(500).json({message: "Server error"})
     }
 })
+
+router.post("/login", async(req, res) => {
+    try {
+        const { email, password } = req.body
+        const user = await User.findOne({ email })
+        if(!user) {
+            res.status(400).json({message: "Invalid email or password!!"})
+        }
+        const isPasswordMatched = await bcrypt.compare(password, user.password)
+        if(!isPasswordMatched){
+            res.status(400).json({message: " Invalid email or password"})
+        }
+
+        const token = jwt.sign(
+            { id: user._id },
+            process.env.JWT_SECRET || "secret123",
+            { expiresIn: "7d" }
+        )
+        res.status(200).json({message: "Login successful",
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email
+            },
+        });
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({message : "Server error"})
+    }
+}) 
 
 export default router
